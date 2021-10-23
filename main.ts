e5e1d505ebd347027d61381b31e0491a2277186e
Oskar Lindgren <2677573+MrOggy85@users.noreply.github.com>
2021-10-23 17:31:45 +0900

    fix

--- main.ts
+++ main.ts
@@ -1,9 +1,11 @@
+import { getIP } from "https://deno.land/x/get_ip@v2.0.0/mod.ts";
+
 const server = Deno.listen({ port: 8080 });
 
 async function handle(conn: Deno.Conn) {
   const httpConn = Deno.serveHttp(conn);
   for await (const requestEvent of httpConn) {
-    const ip = requestEvent.request.headers.get('host')
+    const ip = await getIP({ipv6: true});
 
     await requestEvent.respondWith(
       new Response(`Your IP address is <b>${ip}</b>`, {
